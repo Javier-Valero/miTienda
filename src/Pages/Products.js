41b3c77fa325882a512b9cf7e68362bb9ca267c5
JVO <jvaola@gmail.com>
2022-05-19 18:14:05 +0200

    Centrada tarjeta producto en el Grid

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -17,9 +17,9 @@ const Products = () => {
     <>
       <CssBaseline />
       <div className={classes.root}>
-        <Grid container spacing={3}>
+        <Grid container spacing={3} >
           {products.map((product) => (
-            <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Grid item xs={12} sm={6} md={4} lg={3} align='center' >
               <Product key={product.id} product={product} />
             </Grid>
           ))}
